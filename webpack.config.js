1615647b513359b49d68bda44043e5b17f71aa89
WeTemp <wetemp.ops@gmail.com>
2017-07-08 20:10:06 +0200

    styling experiments

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'assets'),
     filename: 'bundle.js',
-    publicPath: '/',
+    publicPath: '/assets/',
   },
   module: {
     rules: [
